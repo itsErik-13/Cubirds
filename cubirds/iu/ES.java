448579d3ecf286b1c3af924fb2bbd28d982c81a8
itsErik_13 <erik.p.f.2001@gmail.com>
2022-05-05 12:20:12 +0200

    ,

--- cubirds/iu/ES.java
+++ cubirds/iu/ES.java
@@ -10,11 +10,12 @@ public class ES {
     public static Scanner leer = new Scanner(System.in);
 
     public static String pideCadena(String mensaje) {
-        // Poner el mensaje
-        System.out.println(mensaje);
-
-        // Pedir
-        return leer.nextLine();
+        String toRet = "";
+        do {
+            System.out.println(mensaje);
+            toRet = leer.nextLine();
+        } while (toRet.isEmpty());
+        return toRet;
     }
 
     public static int pideNumero(String mensaje) {
